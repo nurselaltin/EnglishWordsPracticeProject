c09af2b698d077726dbc8e167d046f7c9a86ffde
Nursel ALTIN <nurselaltin.na@gmail.com>
2021-09-05 15:03:35 +0300

    Updated  Main method.

--- UI/Console/WordsPracticeConsole/Program.cs
+++ UI/Console/WordsPracticeConsole/Program.cs
@@ -10,105 +10,123 @@ namespace WordsPracticeConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("#####################################################################################");
-            Console.WriteLine("You can see operations which you can do  below.Please choose one of them.");
-            Console.WriteLine("#####################################################################################");
-            Console.WriteLine("Add new Word (Add) or Update word (Update) or Get List Word (GetAll) or Get word by name (GetByName) or  Get word by level (GetByLevel)  ");
-            Console.WriteLine("#####################################################################################");
-            var result = Console.ReadLine();
+          
 
+            bool isKeeping = true;
 
-
-
-            
-            WordManager wordManager = new WordManager(new EnglishWordsBotContext());
-
-
-
-            switch (result.ToString())
+            while (isKeeping)
             {
-                case "add":
 
-                    Word word = new Word();
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("#####################################################################################");
+                Console.WriteLine("You can see operations which you can do  below.Please choose one of them.");
+                Console.WriteLine("#####################################################################################");
+                Console.WriteLine("Add new Word (Add) or Update word (Update) or Get List Word (GetAll) or Get word by name (GetByName) or  Get word by level (GetByLevel)  ");
+                Console.WriteLine("#####################################################################################");
+                var result = Console.ReadLine();
 
-                    Console.WriteLine("Name");
-                    word.Name = Console.ReadLine();
 
-                    Console.WriteLine("Meaning");
-                    word.Meaning = Console.ReadLine();
 
-                    Console.WriteLine("Level");
-                    word.Level = Console.ReadLine();
 
-                    bool AddResult = wordManager.Add(word);
 
-                    if (!AddResult)
-                    {
-                        Console.WriteLine("Error!");
+                WordManager wordManager = new WordManager(new EnglishWordsBotContext());
 
-                    }
-                    else
-                    {
-                        Console.WriteLine("Success!");
-                        var listWord = wordManager.GetAll();
 
-                        foreach (var item in listWord)
+                switch (result.ToString())
+                {
+                    case "add":
+
+                        Word word = new Word();
+
+                        Console.WriteLine("Name");
+                        word.Name = Console.ReadLine().ToLower();
+
+                        Console.WriteLine("Meaning");
+
+                        word.Meaning = Console.ReadLine().ToLower();
+
+                        Console.WriteLine("Level");
+                        word.Level = Console.ReadLine().ToLower();
+
+                        bool AddResult = wordManager.Add(word);
+
+                        if (!AddResult)
                         {
+                            Console.WriteLine("Error!");
 
-                            Console.WriteLine("Name : " + item.Name + "  - Meaning : " + item.Meaning + " - Level : " + item.Level );
-                            Console.WriteLine("-----------------------");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Success!");
+                            var listWord = wordManager.GetAll();
+
+                            foreach (var item in listWord)
+                            {
+
+                                Console.WriteLine("Name : " + item.Name + "  - Meaning : " + item.Meaning + " - Level : " + item.Level);
+                                Console.WriteLine("-----------------------");
+
+
+                            }
+
+                            Console.WriteLine("Do you want to keep to operations? (true / false)");
+                            isKeeping = Convert.ToBoolean(Console.ReadLine());
+
 
 
                         }
-                    }
-                    break;
+                        break;
 
-                case "update":
-
-
-
-                    break;
-
-                case "getall":
-
-                    var list = wordManager.GetAll();
-
-                    foreach (var item in list)
-                    {
-
-                        Console.WriteLine("Name : " + item.Name + "  - Meaning : " + item.Meaning + " - Level : " + item.Level );
-                        Console.WriteLine("-----------------------");
-
-                    }
-
-                    break;
-
-                case "getbyname":
+                    case "update":
 
 
 
-                    break;
+                        break;
 
-                case "getbylevel":
+                    case "getall":
+
+                        var list = wordManager.GetAll();
+
+                        foreach (var item in list)
+                        {
+
+                            Console.WriteLine(item.Name + " : " + item.Meaning + "  : " + item.Level);
+                            Console.WriteLine("-----------------------");
+
+                        }
+
+                        break;
+
+                    case "getbyname":
 
 
 
-                    break;
-                case "":
+                        break;
 
-                    Console.WriteLine("Program shut down...");
-                    Thread.Sleep(100);
-                    Console.Clear();
+                    case "getbylevel":
 
-                    break;
 
+
+                        break;
+                    case "":
+
+                        Console.WriteLine("Program shut down...");
+                        Thread.Sleep(100);
+                        Console.Clear();
+
+                        break;
 
 
 
 
+
+
+                }
 
             }
+
+
+           
 
 
 
